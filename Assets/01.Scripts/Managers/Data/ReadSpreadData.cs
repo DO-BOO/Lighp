c02be51ca434674749cd14cd82ef41f601695306
minyoung529 <dlalsdud102613@gmail.com>
2022-10-15 16:41:42 +0900

    * 시트별로 데이터 구별하는 딕셔너리 생성 * 스프레드 시트 데이터 > 클래스 배열로 변환

--- Assets/01.Scripts/Managers/Data/ReadSpreadData.cs
+++ Assets/01.Scripts/Managers/Data/ReadSpreadData.cs
@@ -2,40 +2,92 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using System.Reflection;
+using System;
+
 /// <summary>
 /// 구글 스프레드시트에서 데이터 실시간으로 읽어오는 스크립트
 /// </summary>
 public class ReadSpreadData : MonoBehaviour
 {
-    // 스프레드시트 링크 (뒷부붕 export부터는 부가 기능, 다른 시트는 URL에 tsv&gid=${id})
-    const string URL = "https://docs.google.com/spreadsheets/d/1fBTpWcRQGfyKeq0S3ZvXEh_r4YQZlY6ELVGRMWtoKbw/export?format=tsv&range=A2:B";
+    // key      > 스프레드 시트 주제
+    // value    > 스프레드시트 데이터 (처음엔 링크)
+    private Dictionary<SheetType, string> sheetDatas = new Dictionary<SheetType, string>();
 
     // 시작 했을 때 URL에서 데이터 읽어서 string에 저장
     IEnumerator Start()
     {
-        UnityWebRequest www = UnityWebRequest.Get(URL);
-        yield return www.SendWebRequest();
+        List<SheetType> sheetTypes = new List<SheetType>(sheetDatas.Keys);
 
-        string data = www.downloadHandler.text;
-        SetData(data);
+        foreach (SheetType type in sheetTypes)
+        {
+            UnityWebRequest www = UnityWebRequest.Get(sheetDatas[type]);
+            yield return www.SendWebRequest();
+
+            sheetDatas[type] = www.downloadHandler.text;
+        }
     }
 
-    // 저장된 string에 탭과 엔터로 값 나누어 데이터 변수에 저장
-    void SetData(string tsv)
+    // sheet 타입에 맞춰 시트 데이터를 T형의 리스트로 만들어주는 함수
+    public List<T> GetDatas<T>(SheetType sheet) where T : new()
     {
-        string[] row = tsv.Split('\n');
+        List<T> list = new List<T>();
+
+        // 탭과 엔터로 값 나누어 데이터 변수에 저장
+        string[] row = sheetDatas[sheet].Split('\n');
         int rowSize = row.Length;
 
         for (int i = 0; i < rowSize; i++)
         {
             string[] column = row[i].Split('\t');
-
-            // 예시로 데이터 대충 집어넣어서 Debug로 확인해본거임
-            Debug.Log(column[0]); // 무기 이름
-            Debug.Log(column[1]); // 속도
-
-            // 여기에다 column[열 인데스 값]으로 데이터 가져올 수 있음
+            list.Add(GetData<T>(column));
         }
+
+        return list;
     }
 
+    // 탭으로 나눠진 데이터를 형식에 따라 T형의 struct, class에 저장함
+    // 시트 순서와 선언된 데이터 순서가 맞아야 함
+    T GetData<T>(string[] column) where T : new()
+    {
+        T data = new T();
+
+        // 클래스에 있는 변수들을 순서대로 저장한 배열
+        FieldInfo[] fields = typeof(T).GetFields(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+        for (int i = 0; i < fields.Length; i++)
+        {
+            if (i >= column.Length) break;
+
+            try
+            {
+                // string > parse
+                Type type = fields[i].FieldType;
+
+                if (type == typeof(int))
+                {
+                    fields[i].SetValue(data, int.Parse(column[i]));
+                }
+                else if (type == typeof(float))
+                {
+                    fields[i].SetValue(data, float.Parse(column[i]));
+                }
+                else if (type == typeof(bool))
+                {
+                    fields[i].SetValue(data, bool.Parse(column[i]));
+                }
+                else // string
+                {
+                    fields[i].SetValue(data, column[i]);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Debug.LogError($"SpreadSheet Error : {e.Message}");
+            }
+        }
+
+        return data;
+    }
 }
