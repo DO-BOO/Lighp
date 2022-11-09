4cb8cc75bfa3b091323321ae66dd7aa61551e03e
dlalsdud102613 <dlalsdud102613@gmail.com>
2022-11-09 23:24:31 +0900

    커스텀 에디터 준비 스크립트

---
+++ Assets/01.Scripts/Managers/Editor/CustomToolBar.cs
@@ -0,0 +1,15 @@
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
+
+//[InitializeOnLoad]
+//public class CustomToolBar
+//{
+//    static void OnToolbarGUI()
+//    {
+//        GUILayout.FlexibleSpace();
+
+//        if(GUILayout.Button(new GUIContent("1", "Game Scene"),))
+//    }
+//}
